a95d095b2452c476f7c0c4404dad5da347251e4b
Tony <antonnikolov2007@gmail.com>
2026-03-09 14:55:44 +0200

    backend structure fix

--- backend/src/main/java/com.anton.sportshop/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anton.sportshop.repository;
-
-import com.anton.sportshop.model.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
--- backend/src/main/java/com.anton.sportshop/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anton.sportshop.repository;
-
-import com.anton.sportshop.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item,Long> {
-}
