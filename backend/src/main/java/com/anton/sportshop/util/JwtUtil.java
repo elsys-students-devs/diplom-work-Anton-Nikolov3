a21b2123b445162582eb5a222cb856a4a0fa714d
Anton-nikolov <100777577+Anton-Nikolov3@users.noreply.github.com>
2026-03-20 17:46:44 +0200

    Enhance JWT token generation with timestamps
    Add token generation with issued and expiration dates.

--- backend/src/main/java/com/anton/sportshop/util/JwtUtil.java
+++ backend/src/main/java/com/anton/sportshop/util/JwtUtil.java
@@ -1,13 +1,13 @@
 package com.anton.sportshop.util;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
@@ -16,8 +16,13 @@ public class JwtUtil {
             Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
 
     public String generateToken(UserDetails user){
+        long now = System.currentTimeMillis();
+        long expirationTime = 1000 * 60 * 60 * 24;
+
         return Jwts.builder()
                 .setSubject(user.getUsername())
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + expirationTime))
                 .signWith(KEY)
                 .compact();
     }
